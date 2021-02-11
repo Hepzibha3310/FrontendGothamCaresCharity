96584c495bb788fb73d3976ec906878dc3ff3bff
Hepzibha3310 <76929242+Hepzibha3310@users.noreply.github.com>
2021-02-11 17:32:50 +0530

    Add files via upload

---
+++ GothamCaresCharitySociety/src/app/admin/admin.component.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css']
+})
+export class AdminComponent implements OnInit {
+  outlets:any;
+
+  constructor(private http:HttpClient) { }
+
+  ngOnInit(): void {
+  }
+ 
+  
+
+}
